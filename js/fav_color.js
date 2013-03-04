866e474d9e624117bc3a9234b750ad2deef29051
Marilyn Rose <marilyn@can.is>
2013-03-04 15:54:09 +0000

    added js commands

--- js/fav_color.js
+++ js/fav_color.js
@@ -4,9 +4,15 @@ $(
   function(event) {
   	//Start to type below here. Make sure not to delete any "{}" braces. 
 
-  	var favColour = prompt ("Whats your fav colour?");
+  	var faveColour = prompt ("Whats your fave colour?");
 
+  if (faveColour == "yellow" || faveColour == "green" || faveColour == "blue" || faveColour ==  "red"){
+  		$("body").css ('background-color', faveColour);
 
+	} else {
+  		$("body").css ('background-color', "white");
+  		$("h2").html("We don't have that colour");
+  }
 
 
 
